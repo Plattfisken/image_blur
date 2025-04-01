d0dfda8816ff8e9c99008fe3cd7f3e1b8113dc84
Plattfisken <158032673+Plattfisken@users.noreply.github.com>
2025-04-01 16:28:23 -0300

    pythonskript som tar emot bilder via cmdline och blurrar ut människor som modellen känner igen och sparar de bilderna på nytt

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+// See https://aka.ms/new-console-template for more information
+foreach(var arg in args) {
+    Console.WriteLine($"arg: {arg}");
+    SaveBytesToFile(Encoding.UTF8.GetBytes(arg));
+}
+
+static string SaveBytesToFile(byte[] bytes) {
+    int numSuffix = 0;
+    var files = Directory.GetFiles(".");
+    foreach(var name in files) {
+        if(name.Contains("temp")) ++numSuffix;
+    }
+    var fileName = $"temp{numSuffix}";
+    var file = File.Open(fileName, FileMode.CreateNew);
+    file.Write(bytes, 0, bytes.Length);
+    file.Close();
+    return fileName;
+}
---
+++ object_detection.py
@@ -0,0 +1,46 @@
+from transformers import DetrImageProcessor, DetrForObjectDetection
+from PIL import Image
+import torch
+import sys
+import cv2
+import datetime
+
+def get_file_extension(file_name):
+    reversed_file_name = file_name[::-1]
+    last_dot_index = reversed_file_name.find(".")
+    reversed_file_extension = reversed_file_name[:last_dot_index]
+    return reversed_file_extension[::-1]
+
+image_file_paths = sys.argv[1::]
+print(image_file_paths)
+images = [Image.open(arg) for arg in sys.argv[1::]]
+
+processor = DetrImageProcessor.from_pretrained("facebook/detr-resnet-50", revision="no_timm")
+model = DetrForObjectDetection.from_pretrained("facebook/detr-resnet-50", revision="no_timm")
+
+inputs = processor(images=images, return_tensors="pt")
+outputs = model(**inputs)
+
+sizes = [image.size[::-1] for image in images]
+
+target_sizes = torch.tensor(sizes)
+
+results = processor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=0.1)
+
+for result, image_path, i in zip(results, image_file_paths, range(0, len(results))):
+    image = cv2.imread(image_path)
+    ksize = (40, 40)
+    for box, label in zip(result["boxes"], result["labels"]):
+        if(model.config.id2label[label.item()] == "person"):
+            # extract coordinates and round from box
+            box = [round(i) for i in box.tolist()]
+            x1, y1, x2, y2 = box[0], box[1], box[2], box[3]
+            # rectangle to blur from image
+            roi = image[y1:y2, x1:x2]
+            # apply rectangle
+            blur = cv2.blur(roi, ksize)
+            # insert blur back into image
+            image[y1:y2, x1:x2] = blur
+    now = datetime.datetime.now()
+    cv2.imwrite(f"{now.year}-{now.month}-{now.day}-{now.hour}-{now.minute}-{now.second}-{now.microsecond}.{get_file_extension(image_path)}", image)
+
