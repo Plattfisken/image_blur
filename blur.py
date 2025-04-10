7999d25642b04dcb048674449d69fb45855754a2
Plattfisken <158032673+Plattfisken@users.noreply.github.com>
2025-04-10 10:32:27 -0300

    få tillbaka två bilder, en där de säkraste resultaten är utsuddade, en där osäkra resultat är highlightade

--- blur.py
+++ blur.py
@@ -13,7 +13,7 @@ def pil_to_cv2(pil_images):
 def cv2_to_pil(cv2_images):
     return [Image.fromarray(cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)) for cv2_img in cv2_images]
 
-def detect_objects(images, threshold):
+def detect_objects(images):
     inputs = processor(images=images, return_tensors="pt")
     outputs = model(**inputs)
 
@@ -21,27 +21,35 @@ def detect_objects(images, threshold):
 
     target_sizes = torch.tensor(sizes)
 
-    results = processor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=threshold)
+    results = processor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=0.1)
     return results
 
-def blur_images(images, results):
+def blur_images(images, results, threshold):
     blurred_images = []
+    uncertain_images = []
     for result, cv2_image in zip(results, pil_to_cv2(images)):
         ksize = (30, 30)
+        uncertain_image = cv2_image.copy()
         for box, label, score in zip(result["boxes"], result["labels"], result["scores"]):
             if(model.config.id2label[label.item()] == "person"):
                 print( f"Detected {model.config.id2label[label.item()]} with confidence" f"{round(score.item(), 3)} at location {box}")
-                # extract coordinates and round from box
                 # NOTE: for some reason the model sometimes spits out negative coordinates at low confidence levels, this seems to trigger an
                 # assertion later in cv2.blur, we therefor use max() to clamp the values to at least zero
                 box = [max(round(i), 0) for i in box.tolist()]
                 x1, y1, x2, y2 = box[0], box[1], box[2], box[3]
                 # rectangle to blur from image
                 roi = cv2_image[y1:y2, x1:x2]
-                # apply blur to rectangle
-                print(f"x1: {x1}, x2: {x2}, y1: {y1}, y2: {y2}")
-                blur = cv2.blur(roi, ksize)
-                # insert blur back into image
-                cv2_image[y1:y2, x1:x2] = blur
+                if(score.item() > threshold):
+                    print(f"confidence {score.item()} higher than threshold {threshold}. Blurring rectangle")
+                    # extract coordinates and round from box
+                    # apply blur to rectangle
+                    blur = cv2.blur(roi, ksize)
+                    # insert blur back into image
+                    cv2_image[y1:y2, x1:x2] = blur
+                elif(score.item() > 0.3):
+                    print(f"confidence {score.item()} lower than threshold {threshold}. higlighting rectangle")
+                    start_point = (x1, y1); end_point = (x2, y2); color = (0, 255, 0); thickness = 2
+                    cv2.rectangle(uncertain_image, start_point, end_point, color, thickness)
+        uncertain_images.append(uncertain_image)
         blurred_images.append(cv2_image)
-    return cv2_to_pil(blurred_images)
+    return (cv2_to_pil(blurred_images), cv2_to_pil(uncertain_images))
--- blur_api.py
+++ blur_api.py
@@ -11,14 +11,22 @@ app = FastAPI()
 @app.post("/")
 def blur_image(threshold: float, image_files: List[UploadFile] = File(...)):
     images = [Image.open(image_file.file) for image_file in image_files]
-    results = blur.detect_objects(images, threshold)
-    blurred_images = blur.blur_images(images, results)
+    file_names = [file.filename for file in image_files]
+    results = blur.detect_objects(images)
+    (blurred_images, uncertain_images) = blur.blur_images(images, results, threshold)
 
     zipped_files = BytesIO()
     with zipfile.ZipFile(zipped_files, mode="w", compression=zipfile.ZIP_DEFLATED) as temp:
-        for image, blurred_image in zip(image_files, blurred_images):
+        temp.mkdir("high_certainty")
+        for file_name, blurred_image in zip(file_names, blurred_images):
             image_as_bytes = BytesIO()
             blurred_image.save(image_as_bytes, "jpeg")
             image_as_bytes.seek(0)
-            temp.writestr(image.filename, image_as_bytes.getvalue())
+            temp.writestr(f"high_certainty/{file_name}", image_as_bytes.getvalue())
+        temp.mkdir("low_certainty")
+        for file_name, uncertain_image in zip(file_names, uncertain_images):
+            image_as_bytes = BytesIO()
+            uncertain_image.save(image_as_bytes, "jpeg")
+            image_as_bytes.seek(0)
+            temp.writestr(f"low_certainty/uncertain_{file_name}", image_as_bytes.getvalue())
     return StreamingResponse(iter([zipped_files.getvalue()]), media_type="application/x-zip-compressed", headers={ "Content-Disposition": f"attachment; filename=images.zip"})
